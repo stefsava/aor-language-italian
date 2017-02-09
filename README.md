10699a9e2db64a982d233ad6c25a981e9ae01560
Stefano Savanelli <stefano@savanelli.it>
2017-02-09 15:39:04 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,28 @@
+# Italian Translations for Admin-on-rest
+
+Italian translations for [admin-on-rest](https://github.com/marmelab/admin-on-rest), the frontend framework for building admin applications on top of REST services.
+
+
+## Installation
+
+```sh
+npm install --save aor-language-italian
+```
+
+## Usage
+
+```js
+import italianMessages from 'aor-language-italian';
+
+const messages = {
+    'it': italianMessages,
+};
+
+<Admin locale="it" messages={messages}>
+  ...
+</Admin>
+```
+
+## License
+
+This translation is licensed under the [MIT Licence](LICENSE).
---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = {
+    aor: {
+        action: {
+            delete: 'Cancella',
+            show: 'Mostra',
+            list: 'Elenco',
+            save: 'Salva',
+            create: 'Crea',
+            edit: 'Modifica',
+            cancel: 'Annulla',
+            refresh: 'Aggiorna',
+            add_filter: 'Aggiuni un filtro',
+            delete_item: 'Elimina %{name}',
+            create_item: 'Crea %{name}',
+            list_item: 'Elenco di %{name}',
+        },
+        input: {
+            image: {
+                upload_several: 'Trascina i files da caricare, oppure clicca per selezionare.',
+                upload_single: 'Trascina il file da caricare, oppure clicca per selezionare.',
+            },
+        },
+        message: {
+            yes: 'Si',
+            no: 'No',
+            are_you_sure: 'Sei sicuro ?',
+            about: 'Informazioni',
+        },
+        navigation: {
+            page_out_of_boundaries: 'Il numero di pagina %{page} è fuori dei limiti',
+            page_out_from_end: 'Fine della paginazione',
+            page_out_from_begin: 'Il numero di pagina deve essere maggiore di 1',
+            page_range_info: '%{offsetBegin}-%{offsetEnd} di %{total}',
+            next: 'Successivo',
+            prev: 'Precedente',
+        },
+        auth: {
+            username: 'Nome utente',
+            password: 'Password',
+            sign_in: 'Login',
+            sign_in_error: 'Autenticazione fallita, riprovare.',
+            logout: 'Disconnessione',
+        },
+        notification: {
+            updated: 'Record aggiornato',
+            created: 'Record creato',
+            deleted: 'Record eliminato',
+            item_doesnt_exist: 'Record inesistente',
+        },
+        validation: {
+            required: 'Campo obbligatorio',
+        },
+    },
+};
